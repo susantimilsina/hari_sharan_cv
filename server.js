6b03265b147a0a679f7753534567bc4d1756bc65
Susan Timilsina <susantimilsina10@gmail.com>
2026-06-24 06:07:49 +0545

    chore: fix server issue

---
+++ server.js
@@ -0,0 +1,14 @@
+const { createServer } = require("http");
+const { parse } = require("url");
+const next = require("next");
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+const port = process.env.PORT || 3000;
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    handle(req, res, parse(req.url, true));
+  }).listen(port);
+});
